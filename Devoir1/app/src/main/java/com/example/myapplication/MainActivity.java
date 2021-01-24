9944baf9848f74a73a7097d4d24e9eae0321c7e6
Benliam12 <s100live.ca@live.ca>
2021-01-24 14:23:18 -0500

    Timer is working

--- Devoir1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Devoir1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -36,7 +36,33 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Long> test = new ArrayList<Long>();
 
     Handler timer = new Handler();
+
+
     public static MainActivity activity = null;
+
+    Runnable chronoR = new Runnable() {
+        @Override
+        public void run() {
+            while(true)
+            {
+
+                try
+                {
+                    Thread.sleep(1);
+                } catch (InterruptedException ex)
+                {
+
+                }
+
+                if(isYellow)
+                {
+                    text2.setText(System.currentTimeMillis()-startime + "ms");
+                }
+            }
+
+        }
+    };
+    Thread chronoT = new Thread(chronoR);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +75,15 @@ public class MainActivity extends AppCompatActivity {
         text2 = findViewById(R.id.textView4);
 
         btn.setOnClickListener(b1_listener);
+
+        chronoT.start();
     }
 
     /**
      * Method used when user presses the button
      */
+
+
 
 
     View.OnClickListener b1_listener = new View.OnClickListener(){
@@ -72,7 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 btn.setBackgroundColor(getResources().getColor(R.color.green));
                 compteur++;
                 btn.setText("");
-                text2.setText("succes ! vous avez clique en" +  elapseTime + "ms");
+                //text2.setText("succes ! vous avez clique en" +  elapseTime + "ms");
+
                 isYellow = false;
                 timer.removeCallbacks(myRunnable);
                 if(compteur < 6){
@@ -94,8 +125,15 @@ public class MainActivity extends AppCompatActivity {
                                     dialog.dismiss();
                                 }
                             });
+                    alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            start();
+                        }
+                    });
                     alertDialog.show();
-                    start();
+
+
                 }
             }
 
@@ -127,10 +165,11 @@ public class MainActivity extends AppCompatActivity {
         started = true;
         int delay = (int) (new Random().nextDouble() * 7000) + 3000;
         Button btn = (Button) findViewById(R.id.button3);
-        btn.setText("Veuillez attendre que le bouton soit jaune pour cliquer");
+        btn.setText(getResources().getText(R.string.ingame_msg));
         btn.setBackgroundColor(getResources().getColor(R.color.gray));
         timer.postDelayed(myRunnable, delay);
     }
+
 
     Runnable myRunnable = new Runnable() {
         @Override
@@ -139,9 +178,5 @@ public class MainActivity extends AppCompatActivity {
             btn.setBackgroundColor(getResources().getColor(R.color.yellow));
             MainActivity.activity.startime = System.currentTimeMillis();
             }
-
     };
-    public void test(){
-
-    }
 }
